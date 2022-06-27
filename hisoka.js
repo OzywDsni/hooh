9a41a1da0ba3836e10149901d2043bdeab96c191
OzywDsni <103554187+OzywDsni@users.noreply.github.com>
2022-06-27 22:49:25 +0700

    Add files via upload

--- hisoka.js
+++ hisoka.js
@@ -2524,11 +2524,14 @@ _CPU Core(s) Usage (${cpus.length} Core CPU)_
 ${cpus.map((cpu, i) => `${i + 1}. ${cpu.model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}`).join('\n\n')}` : ''}
                 `.trim()
                 m.reply(respon)
-                let btn = [{
+                  let btn = [{
                                 urlButton: {
                                     displayText: 'Source Code',
                                     url: 'https://github.com/DikaArdnt/Hisoka-Morou'
-                                }                             
+                                }
+                            }]
+                        hisoka.send5ButImg(m.chat, anu, hisoka.user.name, global.thumb, btn)
+                     }
             break
             case 'speedtest': {
             m.reply('Testing Speed...')
